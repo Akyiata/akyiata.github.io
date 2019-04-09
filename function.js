3e07304f31d34834ea9f611a9e8409582acd8e23
Akyiata <40244477+Akyiata@users.noreply.github.com>
2019-04-09 21:02:11 +0200

    SXUI
    SXUI new update

--- function.js
+++ function.js
@@ -1,48 +1,27 @@
-var i=0;
-
 function checkConsole(){
     if (i === 20) {
-	document.getElementById('smefpw2').innerHTML = "<font face=\"verdana\"><font color=\"red\">[console_<]</font> console reset<br></font>";
+	document.getElementById('qwertz2').innerHTML = "<font face=\"verdana\"><font color=\"rebeccapurple\">SX@UI~#: </font>console reset<br></font>";
 	i=1;
 	}
 }
-
-// disable rightclick
-document.addEventListener("contextmenu", function(disablemouse) {
- disablemouse.preventDefault();
-document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"red\">[console_<]</font> context menu disabled</font><br>";
-i += 1;
-checkConsole();
-}, false);
-// disable rightclick
-
-// urls functions
 function forum() {
-	document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"red\">[console_<]</font> Skidsense -> Loaded</font><br>";
+	document.getElementById('qwertz2').innerHTML += "<font face=\"verdana\"><font color=\"rebeccapurple\">SX@UI~#: </font>forum loaded</font><br>";
 	i += 1;
 	checkConsole();
 }
 
 function discord() {
-	document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"red\">[console_<]</font> Discord -> Loaded</font><br>";
+	document.getElementById('qwertz2').innerHTML += "<font face=\"verdana\"><font color=\"rebeccapurple\">SX@UI~#: </font>discord loaded</font><br>";
 	i += 1;
 checkConsole();
 }
-function uuuu() {
-	document.getElementById('smefpw2').innerHTML += "<font face=\"verdana\"><font color=\"red\">[console_<]</font> uuuu</font><br>";
+function keybase() {
+	document.getElementById('qwertz2').innerHTML += "<font face=\"verdana\"><font color=\"rebeccapurple\">SX@UI~#: </font>discord loaded</font><br>";
+    i += 1;
+    checkConsole();
+}
+function storage() {
+	document.getElementById('qwertz2').innerHTML += "<font face=\"verdana\"><font color=\"rebeccapurple\">SX@UI~#: </font>storage loaded</font><br>";
 	i += 1;
 checkConsole();
 }
-// urls functions
-
-// site checker
-function checkURL() {
-var currentLocation = window.location.href;
-	if (currentLocation == "https://satanax.tk") {
-		document.getElementById('smefpw').innerHTML += "<br><font face=\"verdana\"><font color=\"red\">[console_<]</font> official site<br></font>";
-}	
-else {
-		document.getElementById('smefpw').innerHTML += "<br><font face=\"verdana\"><font color=\"red\">[console_<]</font> unverified site current site " + window.location.href + " differs from official site smef.pw<br></font>";
-	}
-}
-// site checker
